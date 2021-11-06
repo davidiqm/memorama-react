1698ff38a7f5d61f4bd0b3530b0953b8e2cd8788
David Quime <al061737@uacam.mx>
2021-11-05 22:25:28 -0600

    Modificaciones con script de ejecución

--- server.js
+++ server.js
@@ -2,15 +2,15 @@ const express = require('express');
 const favicon = require('express-favicon');
 const path = require('path');
 const port = process.env.PORT || 8080;
-const app = expres();
+const app = express();
 
-app.use(favicon(__dirname + '/build/favicon.ico'));
+app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(express.static(__dirname));
 app.get('/ping', (req, res) => {
     return res.send('pong');
 });
 app.get('/*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 app.listen(port);

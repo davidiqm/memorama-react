c18c8516e076a9239f3b9e8cfd66441b0977afe1
David Quime <al061737@uacam.mx>
2022-06-27 17:55:15 -0500

    Updates

---
+++ precache-manifest.492d647df98148dc128809ae917425fc.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b36ab2282719c9e557c70f6ef04a74d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "221584a232832c020b42",
+    "url": "/static/css/2.9a9b1a2c.chunk.css"
+  },
+  {
+    "revision": "1c36f23d3652a5b2e6d1",
+    "url": "/static/css/main.ef7d596f.chunk.css"
+  },
+  {
+    "revision": "221584a232832c020b42",
+    "url": "/static/js/2.6f5c347a.chunk.js"
+  },
+  {
+    "revision": "28f49bec5ac2dc8ce9a86de5d1b1939e",
+    "url": "/static/js/2.6f5c347a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c36f23d3652a5b2e6d1",
+    "url": "/static/js/main.7538e094.chunk.js"
+  },
+  {
+    "revision": "bbce08982fd0332992fd",
+    "url": "/static/js/runtime-main.ace4cd4c.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/static/media/logo.ee7cd8ed.svg"
+  }
+]);
